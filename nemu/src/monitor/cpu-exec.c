b1553a665368c44f1999d58cc3179f1c9e1e23d6
tracer-ics2015 <tracer@njuics.org>
2015-09-03 21:06:08 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  21:06:08 up  4:37,  1 user,  load average: 0.00, 0.01, 0.05 77e8a101c4359bc46a0d9625c5658605730ef83d

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -60,10 +60,11 @@ void cpu_exec(volatile uint32_t n) {
 		/* Execute one instruction, including instruction fetch,
 		 * instruction decode, and the actual execution. */
 		int instr_len = exec(cpu.eip);
-		printf("asd\n");
+		//printf("asd\n");
 		cpu.eip += instr_len;
 
 #ifdef DEBUG
+		printf("asd");
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
 		Log_write("%s\n", asm_buf);
