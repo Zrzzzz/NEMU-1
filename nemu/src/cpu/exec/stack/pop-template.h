aec9008410dd63c47e4bfdb1be3127472637996d
tracer-ics2015 <tracer@njuics.org>
2015-09-15 13:29:24 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  13:29:24 up 9 days, 17:17,  1 user,  load average: 0.00, 0.01, 0.05 68315004b641a76b6935c9275846a1539a80e5ab

---
+++ nemu/src/cpu/exec/stack/pop-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute() {
+	hwaddr_write(op_dest->addr, DATA_BYTE, hwaddr_read(reg_l(R_ESP), DATA_BYTE)); 
+	reg_l(R_ESP) = reg_l(R_ESP) + DATA_BYTE;
+	print_asm_template1();
+}
+
+make_instr_helper(r)
+make_instr_helper(m)
+#include "cpu/exec/template-end.h"
