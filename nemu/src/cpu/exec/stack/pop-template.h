a0a0a32b0b89df5c6b1b88aeec84f036d01aef4e
tracer-ics2015 <tracer@njuics.org>
2015-10-04 04:20:07 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  04:20:07 up 22:35,  2 users,  load average: 0.00, 0.01, 0.05 586d9efd4e83cefb46f2cbf617a4e75640203dfb

--- nemu/src/cpu/exec/stack/pop-template.h
+++ nemu/src/cpu/exec/stack/pop-template.h
@@ -1,13 +1,21 @@
 #include "cpu/exec/template-start.h"
 
+#if DATA_BYTE == 1
+#define RET_DATA_TYPE int8_t
+#elif DATA_BYTE == 2
+#define RET_DATA_TYPE int16_t
+#elif DATA_BYTE == 4
+#define RET_DATA_TYPE int32_t
+#endif
 #define instr pop
 
 static void do_execute() {
-	hwaddr_write(op_src->addr, DATA_BYTE, hwaddr_read(reg_l(R_ESP), DATA_BYTE)); 
+	OPERAND_W(op_src, (RET_DATA_TYPE) swaddr_read(reg_l(R_ESP), DATA_BYTE)); 
 	reg_l(R_ESP) = reg_l(R_ESP) + DATA_BYTE;
 	print_asm_template1();
 }
 
 make_instr_helper(r)
 make_instr_helper(m)
+#undef RET_DATA_TYPE
 #include "cpu/exec/template-end.h"
