998e5fe6859e0533deddc88332dc68661a0e8081
tracer-ics2015 <tracer@njuics.org>
2015-10-04 03:52:24 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  03:52:24 up 22:07,  2 users,  load average: 0.00, 0.01, 0.05 46e453ea1c33221cba968c550fc196b18830d81

--- nemu/src/cpu/exec/add/add-template.h
+++ nemu/src/cpu/exec/add/add-template.h
@@ -1,16 +1,30 @@
 #include "cpu/exec/template-start.h"
 
 #if DATA_BYTE == 1
-#define RET_DATA_TYPE int16_t
+#define RET_DATA_TYPE int8_t
 #elif DATA_BYTE == 2
-#define RET_DATA_TYPE int32_t
+#define RET_DATA_TYPE int16_t
 #elif DATA_BYTE == 4
-#define RET_DATA_TYPE int64_t
+#define RET_DATA_TYPE int32_t
 #endif
 #define instr add
 
 static void do_execute() {
 	RET_DATA_TYPE res = op_src->val + op_dest->val;
+	if(res >> ((DATA_BYTE << 3) - 1)) {
+		cpu.CF = (op_src->val >> ((DATA_BYTE << 3) - 1)) & (op_dest->val >> ((DATA_BYTE << 3) - 1));
+	}
+	else {
+		cpu.CF = (op_src->val >> ((DATA_BYTE << 3) - 1)) | (op_dest->val >> ((DATA_BYTE << 3) - 1));
+	}
+	cpu.PF = 0;
+	int i;
+	for(i = 0; i < (DATA_BYTE << 3); i ++) {
+		cpu.PF = cpu.PF ^ ((res >> i) & 1);
+	}
+	cpu.ZF = (res == 0);
+	cpu.SF = ((res >> ((DATA_BYTE << 3) - 1)) & 1);
+	cpu.OF = ((res ^ op_src->val) & (res ^ op_dest->val) & 1);
 	OPERAND_W(op_dest, res);
 	print_asm_template2();
 }
