ecacf693b0902964250373f49035c3dde38ab5f2
tracer-ics2015 <tracer@njuics.org>
2015-10-04 07:11:26 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  07:11:26 up 1 day,  1:26,  2 users,  load average: 0.03, 0.02, 0.05 8b7c38a27d6505d139f5f43f13f293fadec700ca

--- nemu/src/cpu/exec/cmp/cmp.h
+++ nemu/src/cpu/exec/cmp/cmp.h
@@ -2,10 +2,14 @@
 #define __CMP_H__
 
 make_helper(cmp_i2rm_b);
-make_helper(cmp_i2rm_w);
-make_helper(cmp_i2rm_l);
+make_helper(cmp_i2a_b);
+make_helper(cmp_r2rm_b);
+make_helper(cmp_rm2r_b);
 
 make_helper(cmp_i2rm_v);
 make_helper(cmp_si2rm_v);
+make_helper(cmp_i2a_v);
+make_helper(cmp_r2rm_v);
+make_helper(cmp_rm2r_v);
 
 #endif
