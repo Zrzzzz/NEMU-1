18cbb0a7c5063daf6ff312f659d6fdae58a136ad
tracer-ics2015 <tracer@njuics.org>
2015-10-04 04:52:41 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  04:52:41 up 23:07,  2 users,  load average: 0.00, 0.01, 0.05 3318785a72a08ddaa07338e0357e99fd69485bd

--- nemu/src/cpu/exec/data-sub/sub.h
+++ nemu/src/cpu/exec/data-sub/sub.h
@@ -1,20 +1,15 @@
 #ifndef __SUB_H__
 #define __SUB_H__
 
-make_helper(sub_i2r_b);
+make_helper(sub_i2a_b);
 make_helper(sub_i2rm_b);
 make_helper(sub_r2rm_b);
 make_helper(sub_rm2r_b);
-//make_helper(sub_i2rml_b);
-make_helper(sub_a2moffs_b);
-make_helper(sub_moffs2a_b);
 
-make_helper(sub_i2r_v);
+make_helper(sub_i2a_v);
 make_helper(sub_i2rm_v);
 make_helper(sub_r2rm_v);
 make_helper(sub_rm2r_v);
 make_helper(sub_si2rm_v);
-make_helper(sub_a2moffs_v);
-make_helper(sub_moffs2a_v);
 
 #endif
