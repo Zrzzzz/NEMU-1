58ae4f6eb82caf1007166f6ce1d19b3a7166f1e1
tracer-ics2015 <tracer@njuics.org>
2015-10-05 15:00:24 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  15:00:24 up  6:27,  1 user,  load average: 0.00, 0.01, 0.05 e903c4dec999a81141ad4220d7b28201e4b46f58

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -3,7 +3,7 @@
 #define instr movzx
 
 static void do_execute() {
-	OPERAND_W(op_dest, hwaddr_read(op_src->val, DATA_BYTE));
+	OPERAND_W(op_dest, swaddr_read(op_src->val, DATA_BYTE));
 	print_asm_template2();
 }
 
