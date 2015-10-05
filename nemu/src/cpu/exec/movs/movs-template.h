16d4b4e4f544ed7158568d368864768b866c66bb
tracer-ics2015 <tracer@njuics.org>
2015-10-06 06:28:24 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  06:28:24 up 21:55,  1 user,  load average: 0.00, 0.01, 0.05 de356082e078910a9c69d591378321b79f7397e

--- nemu/src/cpu/exec/movs/movs-template.h
+++ nemu/src/cpu/exec/movs/movs-template.h
@@ -3,7 +3,7 @@
 #define instr movs
 
 make_helper(concat(movs_, SUFFIX)) {
-	swaddr_write(concat(reg_, SUFFIX)(R_EDI), DATA_BYTE, (DATA_TYPE) reg_l(R_ESI));
+	swaddr_write(concat(reg_, SUFFIX)(R_EDI), DATA_BYTE, (DATA_TYPE) MEM_R(reg_l(R_ESI)));
 	concat(reg_, SUFFIX)(R_EDI) =(DATA_TYPE) reg_l(R_EDI) - ((cpu.DF << 1) - 1) * DATA_BYTE;
 	concat(reg_, SUFFIX)(R_ESI) =(DATA_TYPE) reg_l(R_ESI) - ((cpu.DF << 1) - 1) * DATA_BYTE;
 	print_asm_template1();
