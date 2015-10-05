46f82eb0a920f6f42fd87c4afdc762be48dae669
tracer-ics2015 <tracer@njuics.org>
2015-10-05 17:35:46 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  17:35:46 up  9:03,  1 user,  load average: 0.00, 0.01, 0.05 86ee68689846ea149fb35fe836eb2f911660732e

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -6,11 +6,11 @@ static void do_execute() {
 #if DATA_BYTE == 4
 	reg_l(R_ESP) = reg_l(R_ESP) - DATA_BYTE;
 	swaddr_write(reg_l(R_ESP), DATA_BYTE, cpu.eip + 5);
-	cpu.eip = cpu.eip + op_src->val;
+	cpu.eip = cpu.eip + op_src->imm;
 #elif DATA_BYTE == 2
 	reg_w(R_SP)= reg_w(R_SP)- DATA_BYTE;
 	swaddr_write(reg_w(R_SP), DATA_BYTE, cpu.ip + 3);
-	cpu.eip = cpu.eip + op_src->val;
+	cpu.eip = cpu.eip + op_src->imm;
 	cpu.eip = cpu.eip & 0xffff;
 #endif
 	print_asm_template1();
