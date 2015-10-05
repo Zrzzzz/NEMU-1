cfc894c1a018a63a44ed37cfae82b877c43a351c
tracer-ics2015 <tracer@njuics.org>
2015-10-05 13:08:52 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  13:08:52 up  4:36,  1 user,  load average: 0.11, 0.07, 0.05 787a26a6df80e224f37d3711598e7903a47e9c07

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -7,15 +7,15 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	uint32_t pf = (res & 255);
+	uint32_t pf = (result & 255);
 	pf = (pf >> 4) ^ pf;
 	pf = (pf >> 2) ^ pf;
 	pf = (pf >> 1) ^ pf;
 	cpu.CF = op_dest->val < op_src->val;
 	cpu.PF = (pf & 1);
-	cpu.ZF = (res == 0);
-	cpu.SF = (res >> 31) & 1;
-	cpu.OF = ((op_dest->val >> 31) ^ (op_src->val >> 31)) & ((op_dest->val >> 31) ^ (res >> 31)) & 1;
+	cpu.ZF = (result == 0);
+	cpu.SF = (result >> 31) & 1;
+	cpu.OF = ((op_dest->val >> 31) ^ (op_src->val >> 31)) & ((op_dest->val >> 31) ^ (result >> 31)) & 1;
 
 	print_asm_template1();
 }
