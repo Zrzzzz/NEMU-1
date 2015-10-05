610ff8f3eda5932ad39d590d35bddabebdf15a5b
tracer-ics2015 <tracer@njuics.org>
2015-10-06 05:26:45 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  05:26:45 up 20:54,  1 user,  load average: 0.05, 0.04, 0.05 aa9fb1f9a5564bfc142180e9384362f99bbc8a5

--- nemu/src/cpu/exec/ret/ret-template.h
+++ nemu/src/cpu/exec/ret/ret-template.h
@@ -3,7 +3,7 @@
 #define instr ret
 
 make_helper(ret_i_w) {
-	decode_i_w(eip);
+	decode_i_w(eip + 1);
 	cpu.eip = swaddr_read(reg_l(R_ESP), (4 >> ops_decoded.is_data_size_16)); 
 	reg_l(R_ESP) = reg_l(R_ESP) + (4 >> ops_decoded.is_data_size_16);
 	reg_l(R_ESP) += op_src->val;
