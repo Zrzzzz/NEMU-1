b1709167b20810122c7dec1c80b36001ba96e207
tracer-ics2015 <tracer@njuics.org>
2015-10-05 10:02:01 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  10:02:01 up  1:29,  1 user,  load average: 0.01, 0.02, 0.05 152e7ce0a485082162096114f2ee98209f6668c5

--- nemu/src/cpu/exec/ret/ret.c
+++ nemu/src/cpu/exec/ret/ret.c
@@ -9,7 +9,7 @@
 make_helper(ret) {
 	cpu.eip = swaddr_read(reg_l(R_ESP), (4 >> ops_decoded.is_data_size_16)); 
 	reg_l(R_ESP) = reg_l(R_ESP) + (4 >> ops_decoded.is_data_size_16);
-//	print_asm_template1();
+	print_asm_template1();
 	return 1;
 }
 
