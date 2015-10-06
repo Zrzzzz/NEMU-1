56bd738efbd7e5fc60b7e288d100149986c1e5d0
tracer-ics2015 <tracer@njuics.org>
2015-10-06 20:13:34 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  20:13:34 up 1 day, 11:40,  1 user,  load average: 0.00, 0.01, 0.05 ed0709d1a78c10bbe892de6bb5a864dc5bb365bb

--- nemu/src/cpu/exec/logic/shld-template.h
+++ nemu/src/cpu/exec/logic/shld-template.h
@@ -16,7 +16,7 @@ static void do_execute () {
 		count --;
 	}
 
-	OPERAND_W(op_src2, in);
+	OPERAND_W(op_src2, out);
 
 	print_asm("shld" str(SUFFIX) " %s,%s,%s", op_src->str, op_dest->str, op_src2->str);
 }
