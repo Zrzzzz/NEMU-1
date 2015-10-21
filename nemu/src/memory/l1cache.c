15f24776213a193adaaaf8e0ca45bd64c4963edb
tracer-ics2015 <tracer@njuics.org>
2015-10-21 21:03:41 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  21:03:41 up 16 days, 12:30,  1 user,  load average: 0.01, 0.03, 0.05 31690bc4e0417618e557552fabdb2fb631378999

---
+++ nemu/src/memory/l1cache.c
@@ -0,0 +1,113 @@
+#include "common.h"
+#include "burst.h"
+#include "misc.h"
+#include <stdlib.h>
+
+#define CACHE_BLOCK_WIDTH 6
+#define CACHE_WAY_WIDTH 3
+#define CACHE_SET_WIDTH 7
+#define CACHE_TAG_WIDTH (27 - CACHE_SET_WIDTH - CACHE_BLOCK_WIDTH)
+
+typedef union {
+	struct {
+		uint32_t block	: CACHE_BLOCK_WIDTH;
+		uint32_t set	: CACHE_SET_WIDTH;
+		uint32_t tag	: CACHE_TAG_WIDTH;
+	};
+	uint32_t addr;
+} l1cache_addr;	
+
+#define CACHE_BLOCK_SIZE (1 << CACHE_BLOCK_WIDTH)
+#define CACHE_WAY_SIZE (1 << CACHE_WAY_WIDTH)
+#define CACHE_TAG_SIZE (1 << CACHE_TAG_WIDTH) 
+#define CACHE_SET_SIZE (1 << CACHE_SET_WIDTH)
+#define CACHE_BLOCK_MASK (CACHE_BLOCK_SIZE - 1)
+
+typedef struct {
+	uint8_t buf[CACHE_BLOCK_SIZE];
+	int32_t tag;
+	bool valid;
+} l1CB;
+
+l1CB l1cache[CACHE_SET_SIZE][CACHE_WAY_SIZE];
+
+uint32_t dram_read(hwaddr_t addr, size_t len);
+void dram_write(hwaddr_t addr, size_t len, uint32_t data);
+
+void init_l1cache() {
+	int i, j;
+	for(i = 0; i < CACHE_SET_SIZE; i ++) {
+		for(j = 0; j < CACHE_WAY_SIZE; j ++) {
+			l1cache[i][j].valid = false;
+		}
+	}
+}
+
+uint8_t check_cache(hwaddr_t addr) {
+	l1cache_addr temp;
+	temp.addr = addr;
+	uint32_t tag = temp.tag;
+	uint32_t set = temp.set;
+	uint8_t way = 0;
+	int i;
+	bool success = false;
+	for(way = 0; way < CACHE_WAY_SIZE; way ++)
+	if(l1cache[set][way].valid && (l1cache[set][way].tag == tag)) {
+		success = true;
+		break;
+	}
+
+	if(!success) {
+		uint8_t temp1[CACHE_BLOCK_SIZE];
+		hwaddr_t addr_temp = addr & ~CACHE_BLOCK_MASK;
+		for(i = 0;i < CACHE_BLOCK_SIZE;i++) {
+			temp1[i] = (uint8_t)(dram_read(addr_temp + i , 1) & 0xff);
+		}
+		way = (rand() * CACHE_WAY_SIZE) >> CACHE_WAY_WIDTH;
+		memcpy(l1cache[set][way].buf, temp1, CACHE_BLOCK_SIZE);
+		l1cache[set][way].valid = true;
+		l1cache[set][way].tag = tag;
+		Log("Miss");
+	}	
+	return way;
+}
+
+uint32_t l1cache_read(hwaddr_t addr, size_t len) {
+	assert(len == 1 || len == 2 || len == 4);
+	l1cache_addr temp;
+	temp.addr = addr;
+
+	uint32_t set = temp.set;
+	uint8_t way = check_cache(addr);
+	uint32_t block = temp.block;
+
+	uint8_t buf[CACHE_BLOCK_SIZE << 1];
+	memcpy(buf, l1cache[set][way].buf, CACHE_BLOCK_SIZE);
+	if(block + len >= CACHE_BLOCK_SIZE) {
+		temp.addr += CACHE_BLOCK_SIZE;
+		set = temp.set;
+		way = check_cache(addr);
+		memcpy(buf + CACHE_BLOCK_SIZE, l1cache[set][way].buf, CACHE_BLOCK_SIZE);
+	}
+	return *(uint32_t *)(buf + block) & (~0u >> ((4 - len) << 3));
+}
+
+void l1cache_write(hwaddr_t addr, size_t len, uint32_t data) {
+	l1cache_addr temp;
+	temp.addr = addr;
+	uint32_t tag = temp.tag;
+	uint32_t set = temp.set;
+	uint32_t block = temp.block;
+	uint8_t way = 0;
+	bool success = false;
+	for(way = 0; way < CACHE_WAY_SIZE; way ++)
+	if(l1cache[set][way].valid && (l1cache[set][way].tag == tag)) {
+		success = true;
+		break;
+	}
+	dram_write(addr, len, data);
+	if(success){
+		memcpy(l1cache[set][way].buf + block, &data, len);
+	}
+	return;
+}
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -2,15 +2,19 @@
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
+uint32_t l1cache_read(hwaddr_t, size_t);
+void l1cache_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	return l1cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	dram_write(addr, len, data);
+	l1cache_write(addr, len, data);
+	//dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
