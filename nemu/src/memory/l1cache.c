c17fd065a4c8362ccfd558d034b97ae42def9bc9
tracer-ics2015 <tracer@njuics.org>
2015-10-22 01:21:05 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  01:21:05 up 16 days, 16:48,  1 user,  load average: 0.05, 0.06, 0.06 ad209fe330669cce3c6c4a6bd75ede65c9019fe9

--- nemu/src/memory/l1cache.c
+++ nemu/src/memory/l1cache.c
@@ -58,8 +58,8 @@ uint8_t check_cache(hwaddr_t addr) {
 		}
 
 	if(!success) {
-		//for(way = 0; way < CACHE_WAY_SIZE; way ++)
-		//	if(!l1cache[set][way].valid) return way;
+		for(way = 0; way < CACHE_WAY_SIZE; way ++)
+			if(!l1cache[set][way].valid) return way;
 		uint8_t temp1[CACHE_BLOCK_SIZE];
 		hwaddr_t addr_temp = addr & ~CACHE_BLOCK_MASK;
 		for(i = 0;i < CACHE_BLOCK_SIZE;i++) {
