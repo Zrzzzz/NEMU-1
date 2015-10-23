63d8490951afb25624330e8886415cea3576c753
tracer-ics2015 <tracer@njuics.org>
2015-10-24 00:16:42 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  00:16:42 up 18 days, 15:43,  1 user,  load average: 0.01, 0.02, 0.05 3fa0e77e2740bc43cb59620caa4ac82b974cb120

--- nemu/src/cpu/exec/lgdt/lgdt.c
+++ nemu/src/cpu/exec/lgdt/lgdt.c
@@ -3,14 +3,12 @@
 /* for instruction encoding overloading */
 
 make_helper(lgdt) {
+	decode_rm_w(cpu.eip + 1);
 	cpu.gdtr.limit = (uint16_t)op_src->val;
+	decode_rm_w(cpu.eip + 5);
 	cpu.gdtr.base = (uint32_t)op_src->val;
-	printf("%x\n",cpu.eip);
-	assert(0);
-	cpu.eip = swaddr_read(reg_l(R_ESP), (4 >> ops_decoded.is_data_size_16)); 
-	reg_l(R_ESP) = reg_l(R_ESP) + (4 >> ops_decoded.is_data_size_16);
 	print_asm_template1();
-	return 1;
+	return 10;
 }
 
 
