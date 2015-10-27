26bca77a5af2ef0ab3fe758ddbafc267b67944be
tracer-ics2015 <tracer@njuics.org>
2015-10-27 11:08:27 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  11:08:27 up 22 days,  2:35,  1 user,  load average: 0.08, 0.03, 0.05 abf47ab78ca5b4fed3fe9126e6c3338ca9931f24

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,35 +1,31 @@
 #include "common.h"
 #include "cpu/reg.h"
 
-uint32_t dram_read(hwaddr_t, size_t);
-void dram_write(hwaddr_t, size_t, uint32_t);
 uint32_t l1cache_read(hwaddr_t, size_t);
 void l1cache_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	printf("hw%x\n",addr);
 	return l1cache_read(addr, len) & (~0u >> ((4 - len) << 3));
-	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	l1cache_write(addr, len, data);
-	//dram_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	printf("ln%x\n",addr);
 	hwaddr_t hwaddr = page_translate(addr);
 	return hwaddr_read(hwaddr, len);
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
+#ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
+#endif
 	hwaddr_t hwaddr = page_translate(addr);
 	hwaddr_write(hwaddr, len, data);
 }
@@ -38,7 +34,6 @@ uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	printf("sw%x\n",addr);
 	lnaddr_t lnaddr = seg_translate(addr, len, sreg);
 	return lnaddr_read(lnaddr, len);
 }
