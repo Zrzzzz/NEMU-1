e7a764522c39e47627506ae6e043eb530470af9d
tracer-ics2015 <tracer@njuics.org>
2015-11-07 07:40:13 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  07:40:13 up 32 days, 23:07,  1 user,  load average: 0.00, 0.01, 0.05 76ded96ca27b629abff23e4cd64ba4c97c737f2d

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -153,7 +153,7 @@ helper_fun opcode_table [256] = {
 /* 0xe0 */	inv, inv, inv, jcxz_i_v,
 /* 0xe4 */	inv, inv, inv, inv,
 /* 0xe8 */	call_i_v, jmp_i_v, ljmp, jmp_i_b,
-/* 0xec */	in_b, in_v, inv, inv,
+/* 0xec */	in_b, in_v, out_b, out_v,
 /* 0xf0 */	inv, inv, rep, rep,
 /* 0xf4 */	hlt, inv, group3_b, group3_v,
 /* 0xf8 */	inv, inv, cli, inv,
