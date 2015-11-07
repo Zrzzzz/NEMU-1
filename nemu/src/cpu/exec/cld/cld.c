e9ab4e6f9c05f8d9272ae32c65921209dd91f79d
tracer-ics2015 <tracer@njuics.org>
2015-11-07 12:22:08 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  12:22:08 up 33 days,  3:49,  1 user,  load average: 0.00, 0.01, 0.05 f24de5b6210348a3ebf91ffb5b1ff9e791e11151

--- nemu/src/cpu/exec/cld/cld.c
+++ nemu/src/cpu/exec/cld/cld.c
@@ -2,10 +2,12 @@
 
 int cld(uint32_t eip) {
 	cpu.DF = 0;
+	print_asm("cld");
 	return 1;
 }
 
 int sti(uint32_t eip) {
 	cpu.IF = 1;
+	print_asm("sti");
 	return 1;
 }
--- nemu/src/cpu/exec/int/int.c
+++ nemu/src/cpu/exec/int/int.c
@@ -10,7 +10,7 @@ void raise_intr(uint8_t NO) {
 	 */
 	push_val_l(cpu.eflags);
 	push_val_l(cpu.cs.val);
-	push_val_l(cpu.eip + 2);
+	push_val_l(cpu.eip);
 	cpu.cs.val = lnaddr_read(cpu.idtr.base + (NO << 3) + 2, 2);
 	cpu.eip = (lnaddr_read(cpu.idtr.base + (NO << 3) + 6, 2) << 16) + lnaddr_read(cpu.idtr.base + (NO << 3), 2);	
 
@@ -23,7 +23,7 @@ make_helper(intt) {
 	print_asm("intt");
 	raise_intr(op_src->val);
 
-	return 0;
+	return 2;
 }
 
 uint32_t pop_val_l();
