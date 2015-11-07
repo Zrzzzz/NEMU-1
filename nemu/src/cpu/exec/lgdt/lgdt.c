d73f6ee2c9cda18e36b9db17fe12be8ad4775c05
tracer-ics2015 <tracer@njuics.org>
2015-11-08 02:15:45 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  02:15:45 up 33 days, 17:43,  2 users,  load average: 0.01, 0.03, 0.05 c97a7c4e265cdc81de282c0a142896c0c90cd726

--- nemu/src/cpu/exec/lgdt/lgdt.c
+++ nemu/src/cpu/exec/lgdt/lgdt.c
@@ -4,11 +4,8 @@
 
 make_helper(lgdt) {
 	int len = decode_rm_l(eip + 1);
-	printf("asd\n");
 	int limit = lnaddr_read(op_src->addr, 2);
-	printf("asd\n");
 	cpu.gdtr.base = lnaddr_read(op_src->addr + 2, 4);
-	printf("asd\n");
 	cpu.gdtr.limit = limit;
 	print_asm_template1();
 	return 1 + len;
--- nemu/src/cpu/exec/lidt/lidt.c
+++ nemu/src/cpu/exec/lidt/lidt.c
@@ -4,8 +4,8 @@
 
 make_helper(lidt) {
 	int len = decode_rm_l(eip + 1);
-	cpu.idtr.limit = hwaddr_read(op_src->addr, 2);
-	cpu.idtr.base = hwaddr_read(op_src->addr + 2, 4);
+	cpu.idtr.limit = lnaddr_read(op_src->addr, 2);
+	cpu.idtr.base = lnaddr_read(op_src->addr + 2, 4);
 	print_asm_template1();
 	return 1 + len;
 }
