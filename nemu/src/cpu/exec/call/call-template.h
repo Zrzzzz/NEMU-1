5dbd5c502d186484c5ef2356dd39b94ba236058e
tracer-ics2015 <tracer@njuics.org>
2015-11-07 11:31:56 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  11:31:56 up 33 days,  2:59,  1 user,  load average: 0.00, 0.01, 0.05 b0b3a0d7210d4c6fcc7a365af0608a785d84dba

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -2,26 +2,22 @@
 
 #define instr call
 
+void concat(push_val_, SUFFIX)(DATA_TYPE);
+
+make_helper(concat(call_rm_, SUFFIX)) {
+	concat(push_val_, SUFFIX)(cpu.eip + concat(decode_rm_, SUFFIX)(cpu.eip + 1) + 1);
+	cpu.eip = op_src->val;
+	return 0;
+}
+
 static void do_execute() {
-#if DATA_BYTE == 4
-	reg_l(R_ESP) = reg_l(R_ESP) - DATA_BYTE;
-	swaddr_write(reg_l(R_ESP), DATA_BYTE, cpu.eip + decode_rm_l(cpu.eip + 1) + 1, SR_SS);
-	if(instr_fetch(cpu.eip, 1) == 0xff)
-		cpu.eip = op_src->val - decode_rm_l(cpu.eip + 1) - 1;
-	else
-		cpu.eip = cpu.eip + op_src->val;
-#elif DATA_BYTE == 2
-	reg_w(R_SP)= reg_w(R_SP)- DATA_BYTE;
-	swaddr_write(reg_w(R_SP), DATA_BYTE, cpu.ip + decode_rm_w(cpu.eip + 1) + 1, SR_SS);
-	if(instr_fetch(cpu.eip, 1) == 0xff)
-		cpu.eip = op_src->val - decode_rm_w(cpu.eip + 1) - 1;
-	else
-		cpu.eip = cpu.eip + op_src->val;
+	concat(push_val_, SUFFIX)(cpu.eip + DATA_BYTE + 1);
+	cpu.eip = cpu.eip + op_src->val;
+#if DATA_BYTE == 2
 	cpu.eip = cpu.eip & 0xffff;
 #endif
 	print_asm_template1();
 }
 
 make_instr_helper(i)
-make_instr_helper(rm)
 #include "cpu/exec/template-end.h"
