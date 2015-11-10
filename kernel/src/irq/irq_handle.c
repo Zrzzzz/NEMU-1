38bcf648b6b137b34f74a250878ccb27f0853af0
141250011-Chen Jijin <438459483@qq.com>
2015-11-11 07:48:36 +0800

    fix a bug.

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -40,8 +40,6 @@ void irq_handle(TrapFrame *tf) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		do_syscall(tf);
-//	} else if (irq == 0xff) {
-//		handles[1]->routine();
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
 	} else if (irq >= 1000) {
