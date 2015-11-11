2fd9a4367370e1c4df6f0ccad9592c4c41da6773
tracer-ics2015 <tracer@njuics.org>
2015-11-12 00:39:43 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  00:39:43 up 37 days, 16:07,  2 users,  load average: 0.00, 0.01, 0.06 d991ec0ffa8e520bc74dff0bfde025f6bf3caba

--- nemu/src/cpu/exec/data-mov/movzx.c
+++ nemu/src/cpu/exec/data-mov/movzx.c
@@ -8,10 +8,5 @@
 #include "movzx-template.h"
 #undef DATA_BYTE
 
-#define DATA_BYTE 4
-#include "movzx-template.h"
-#undef DATA_BYTE
-
 /* for instruction encoding overloading */
 
-make_helper_v(movzx_rm2r)
