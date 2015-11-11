ed04d0d9aeeb62d31baa252599e00c6e613b82eb
tracer-ics2015 <tracer@njuics.org>
2015-11-11 18:28:00 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  18:28:00 up 37 days,  9:55,  2 users,  load average: 0.00, 0.05, 0.05 6ebb2b7ecf159f43279210364bdd14b6734a6333

--- nemu/src/cpu/exec/adc/adc-template.h
+++ nemu/src/cpu/exec/adc/adc-template.h
@@ -4,15 +4,15 @@
 
 static void do_execute() {
 	op_src->val += cpu.CF;
-	DATA_TYPE_S res = op_src->val + op_dest->val;
-	cpu.CF = res < op_src->val || res < op_dest->val;
+	DATA_TYPE res = op_src->val + op_dest->val;
+	cpu.CF = res < op_src->val;
 	uint32_t pf = (res & 255);
 	pf = (pf >> 4) ^ pf;
 	pf = (pf >> 2) ^ pf;
 	pf = (pf >> 1) ^ pf;
 	cpu.PF = !(pf & 1);
 	cpu.ZF = (res == 0);
-	cpu.SF = ((res >> ((DATA_BYTE << 3) - 1)) & 1);
+	cpu.SF = ((res >> 31) & 1);
 	cpu.OF = ((((res ^ op_src->val) & (res ^ op_dest->val)) >> 31) & 1);
 	OPERAND_W(op_dest, res);
 	print_asm_template2();
