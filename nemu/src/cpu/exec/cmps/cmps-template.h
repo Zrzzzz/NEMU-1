6f772f179b648a67b94a5c26e0508d90a01f2820
tracer-ics2015 <tracer@njuics.org>
2015-11-13 02:02:56 +0800

    >  compile NEMU 141250011 wzcjj Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u2 (2015-07-17) i686 GNU/Linux  02:02:56 up  2:42,  2 users,  load average: 0.00, 0.01, 0.05 7c4616a8fed5b8ff54f32ada8eaf97bea052746

--- nemu/src/cpu/exec/cmps/cmps-template.h
+++ nemu/src/cpu/exec/cmps/cmps-template.h
@@ -3,16 +3,18 @@
 #define instr cmps
 
 make_helper(concat(cmps_, SUFFIX)) {
-	uint32_t res = REG(R_ESI) - REG(R_EDI);
+	DATA_TYPE dest = swaddr_read(reg_l(R_EDI), DATA_BYTE, SR_ES);
+	DATA_TYPE src = swaddr_read(reg_l(R_ESI), DATA_BYTE, SR_DS);
+	DATA_TYPE res = dest - src;
 	uint32_t pf = (res & 255);
 	pf = (pf >> 4) ^ pf;
 	pf = (pf >> 2) ^ pf;
 	pf = (pf >> 1) ^ pf;
-	cpu.CF = REG(R_ESI) > REG(R_EDI);
+	cpu.CF = dest < src;
 	cpu.PF = !(pf & 1);
 	cpu.ZF = (res == 0);
 	cpu.SF = (res >> ((DATA_BYTE << 3) - 1)) & 1;
-	cpu.OF = ((REG((R_ESI)) >> ((DATA_BYTE << 3) - 1)) ^ (REG((R_EDI)) >> ((DATA_BYTE << 3) - 1))) & ((REG((R_ESI)) >> ((DATA_BYTE << 3) - 1)) ^ (res >> ((DATA_BYTE << 3) - 1))) & 1;
+	cpu.OF = ((dest >> ((DATA_BYTE << 3) - 1)) ^ (src >> ((DATA_BYTE << 3) - 1))) & ((dest >> ((DATA_BYTE << 3) - 1)) ^ (res >> ((DATA_BYTE << 3) - 1))) & 1;
 	reg_l(R_ESI) -= (((DATA_TYPE)cpu.DF << 1) - 1) * DATA_BYTE;
 	reg_l(R_EDI) -= (((DATA_TYPE)cpu.DF << 1) - 1) * DATA_BYTE;
 	print_asm_template1();
